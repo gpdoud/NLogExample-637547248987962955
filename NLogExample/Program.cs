fe9c07d1a788be2e979c8f379507c123c58c588f
Greg Doud <gpdoud@gmail.com>
2021-04-22 21:48:21 -0400

    Committed by GitUtil

---
+++ NLogExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using NLog;
+using System;
+
+namespace NLogExample {
+
+    class Program {
+    
+        static void Main(string[] args) {
+
+            var logger = LogManager.GetCurrentClassLogger();
+            var config = new ConfigurationBuilder()
+                            // from Microsoft.Extensions.Configuration.Json package
+                            .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                            .Build();
+            logger.Info("This is an info message");
+            logger.Warn("This is a warning message");
+            logger.Error("This is an error message");
+            LogManager.Shutdown();
+            Console.Write("press any key ... ");
+            Console.ReadKey();
+        }
+    }
+}
